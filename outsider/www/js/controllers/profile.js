4e4307977d66ba21594033e06e061cbd7d17fe25
Hernan Maschwitz <hernanm992@gmail.com>
2015-09-13 19:09:48 -0300

    remove round buttons

--- outsider/www/js/controllers/profile.js
+++ outsider/www/js/controllers/profile.js
@@ -1,3 +1,27 @@
-/**
- * Created by hmaschwitz on 9/13/15.
- */
+angular.module('starter.controllers', [])
+.controller('ProfileCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+    // Set Header
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = false;
+    $scope.$parent.setExpanded(false);
+    $scope.$parent.setHeaderFab(false);
+
+    $scope.user = {name: "Shaun White"}
+
+    // Set Motion
+    $timeout(function() {
+        ionicMaterialMotion.slideUp({
+            selector: '.slide-up'
+        });
+    }, 300);
+
+    $timeout(function() {
+        ionicMaterialMotion.fadeSlideInRight({
+            startVelocity: 3000
+        });
+    }, 700);
+
+    // Set Ink
+    ionicMaterialInk.displayEffect();
+})
