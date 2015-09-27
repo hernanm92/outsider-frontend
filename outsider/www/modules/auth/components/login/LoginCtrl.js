35a1cf3adaf79469c6e20a49da21d95cbfd2a30e
joaquinbucca <buccajoaquin@gmail.com>
2015-09-26 22:49:05 -0300

    frontend login simulated

--- outsider/www/modules/auth/components/login/LoginCtrl.js
+++ outsider/www/modules/auth/components/login/LoginCtrl.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('main')
-    .controller('LoginCtrl', function($scope, $timeout, $stateParams, ionicMaterialInk) {
+    .controller('LoginCtrl', function($scope, $state, $timeout, $stateParams, ionicMaterialInk) {
         ionicMaterialInk.displayEffect();
+
+        $scope.auth= {};
+
+        $scope.loginClick = function(){
+            //todo: call service with auth object;
+            //gAuth.login(auth, function (){
+            $state.go('main.profile');//todo: put params
+            // }, function(response) {
+            // $scope.errorMessage= response;
+            //}
+        }
     });
